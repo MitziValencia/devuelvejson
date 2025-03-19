2975d44030efbe8ca18cf46569bfdd59c86846d7
MitziValencia <mitzivalencia10526@gmail.com>
2025-03-18 23:46:54 -0600

    Add files via upload

---
+++ devuelve.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . "/../lib/php/devuelveJson.php";
+
+devuelveJson([
+    "nombre" => "The Weeknd",
+    "genero" => "R&B, Pop",
+    "album_mas_vendido" => "After Hours"
+]);
